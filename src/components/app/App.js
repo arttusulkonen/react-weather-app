fed116b32d7f4fc8874de6397fe7f9272714f44b
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-02-10 12:23:01 +0200

    Create state and function getWeather for logic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,44 @@
+import React, {Component} from 'react';
 import './App.css';
 import InputForm from "../input-form";
 import WeatherData from "../weather-data";
 
+//get api-key
+const API = '6e1acbd3d9d9c85dca6b27db42890fff';
 
-function App() {
-  return (
-    <div className="App">
-      <InputForm />
-      <WeatherData />
-    </div>
-  );
+export default class App extends Component {
+
+    //create state and declare the variables that will be used
+    state = {
+        temp: null,
+        temp_max: null,
+        temp_min: null,
+        description: '',
+        city: '',
+        country: '',
+        error: '',
+        cod: '',
+        icon: '',
+    }
+
+    // Create a function that contains the application logic
+    getWeather = async () => {
+        //get a response from the server
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=${API}&units=metric`);
+
+        //save the response from the server to data
+        const data = await res.json();
+    }
+
+    render(){
+        return (
+            <div className='container'>
+                <div className="App">
+                    <InputForm />
+                    <WeatherData />
+                </div>
+            </div>
+        );
+    }
+
 }
-
-export default App;
